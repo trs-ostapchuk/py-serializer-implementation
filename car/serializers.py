e86ee98185ac7fc72944338f99fb51729e508d3a
Taras <tarasostapchuk27@gmail.com>
2025-12-18 21:17:16 +0100

    Correct flake

--- car/serializers.py
+++ car/serializers.py
@@ -1,4 +1,3 @@
-from django.core.validators import MinValueValidator, MaxValueValidator
 from rest_framework import serializers
 
 from car.models import Car
@@ -22,10 +21,18 @@ class CarSerializer(serializers.Serializer):
         return Car.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.manufacturer = validated_data.get("manufacturer", instance.manufacturer)
+        instance.manufacturer = validated_data.get(
+            "manufacturer", instance.manufacturer
+        )
         instance.model = validated_data.get("model", instance.model)
-        instance.horse_powers = validated_data.get("horse_powers", instance.horse_powers)
-        instance.is_broken = validated_data.get("is_broken", instance.is_broken)
-        instance.problem_description = validated_data.get("problem_description", instance.problem_description)
+        instance.horse_powers = validated_data.get(
+            "horse_powers", instance.horse_powers
+        )
+        instance.is_broken = validated_data.get(
+            "is_broken", instance.is_broken
+        )
+        instance.problem_description = validated_data.get(
+            "problem_description", instance.problem_description
+        )
         instance.save()
         return instance
